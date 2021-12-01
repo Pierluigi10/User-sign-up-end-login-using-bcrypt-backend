6c989ec78546275fec1db167e8f4d524a2276487
pierluigi <pierluigibaiano@gmail.com>
2021-12-01 14:25:52 +0100

    install and import express-session and cookie-parser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 import express from "express";
 import cors from "cors";
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import { usersRouter } from "./router/usersRouter.js";
 
 const app = express();
 const port = process.env.PORT || 3033;
+
+app.use(cookieParser());
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: "secret"
+    })
+);
 
 app.use(express.json());
 app.use(cors());
